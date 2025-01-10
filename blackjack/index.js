a59e355340a3c71cccc69d5ddd89166f28f68fe5
Rachel Marquant <marquantrachel@gmail.com>
2025-01-10 20:30:19 +0100

    Push

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,23 +1,28 @@
 
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let message ="";
 
-if (sum <= 20)
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+
+function startGame(){
+    sumEl.textContent = "Sum : " + sum;
+    if (sum <= 20){
+        message = "Do you want to draw a new card?";
+    }
+    else if (sum == 21){
+        message = "Woho! You've got a Blackjack!";
+        hasBlackJack = true;
+    }
+    else 
 {
-    message = "Do you want to draw a new card?😁";
-}
-else if (sum == 21)
-{
-    message = "Woho! You've got a Blackjack! 🥳";
-    hasBlackJack = true;
-}
-else 
-{
-    message = "You're out of the game 😭";
-    isAlive = false;
+        message = "You're out of the game";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
 }
 
-console.log(message);
